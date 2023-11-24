64a88b66abf714748bc9953aa66a960e46e4d868
Pieters-Program <maxbezemer@hotmail.nl>
2023-11-24 10:28:59 +0100

    OAuth fix

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -11,34 +11,34 @@ export default function OAuth() {
     try {
       const provider = new GoogleAuthProvider();
       const auth = getAuth(app);
-      
+
       const result = await signInWithPopup(auth, provider);
 
       const res = await fetch('/api/auth/google', {
         method: 'POST',
-        header: {
+        headers: {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-          name: result.user.displayName, 
-          email: result.user.email, 
-          photo: result.user.photoURL 
+          name: result.user.displayName,
+          email: result.user.email,
+          photo: result.user.photoURL,
         }),
       });
       const data = await res.json();
       dispatch(signInSuccess(data));
       navigate('/');
     } catch (error) {
-      console.log('Could not sign in with Google', error);
+      console.log('could not sign in with google', error);
     }
   };
   return (
-    <button 
-      onClick={handleGoogleClick} 
-      type='button' 
+    <button
+      onClick={handleGoogleClick}
+      type='button'
       className='bg-red-700 text-white p-3 rounded-lg uppercase hover:opacity-95'
     >
-      Continue with Google
+      Continue with google
     </button>
-  )
+  );
 }
