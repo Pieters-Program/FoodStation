fd6dc5e29764e1d59b0cc848c6dc3761ab46ca87
Pieters-Program <maxbezemer@hotmail.nl>
2023-11-28 11:29:14 +0100

    Create API listing route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -3,7 +3,7 @@ import User from '../models/user.model.js'
 import {
     errorHandler
 } from '../utils/error.js';
-
+import Listing from '../models/listing.model.js';
 
 
 export const test = (req, res) => {
@@ -54,3 +54,16 @@ res.status(200).json('User has been deleted');
     next(error);
 }
 }
+
+export const getUserListings = async (req, res, next) => {
+    if (req.user.id === req.params.id) {
+      try {
+        const listings = await Listing.find({ userRef: req.params.id });
+        res.status(200).json(listings);
+      } catch (error) {
+        next(error);
+      }
+    } else {
+      return next(errorHandler(401, 'You can only view your own listings'));
+    }
+  };
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -2,7 +2,8 @@ import express from 'express';
 import {
     deleteUser,
     test,
-    updateUser
+    updateUser,
+    getUserListings
 } from '../controllers/user.controller.js';
 import {
     verifyToken
@@ -14,5 +15,6 @@ const router = express.Router();
 router.get('/test', test);
 router.post('/update/:id', verifyToken, updateUser)
 router.delete('/delete/:id', verifyToken, deleteUser)
+router.get('/listings/:id', verifyToken, getUserListings)
 
 export default router
