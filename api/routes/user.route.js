67222ddc8eca4acd25eb5418c7391f09ba96dd8b
Pieters-Program <maxbezemer@hotmail.nl>
2023-12-04 20:09:54 +0100

    Contact landlord functionality

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -3,7 +3,8 @@ import {
     deleteUser,
     test,
     updateUser,
-    getUserListings
+    getUserListings,
+    getUser
 } from '../controllers/user.controller.js';
 import {
     verifyToken
@@ -16,5 +17,6 @@ router.get('/test', test);
 router.post('/update/:id', verifyToken, updateUser)
 router.delete('/delete/:id', verifyToken, deleteUser)
 router.get('/listings/:id', verifyToken, getUserListings)
+router.get('/:id', verifyToken, getUser)
 
 export default router
